9b57974f9493bd48f7fc5e9ebdec74dc420db0bf
Vardan Akopian <vakopian@sustain.net>
2016-01-08 15:26:14 -0800

    babel 6.4 requires semicolons after a class property

--- src/config.js
+++ src/config.js
@@ -1,34 +1,34 @@
 class Config {
 
-    DEFAULT_FORMAT = "x"
+    DEFAULT_FORMAT = "x";
 
-    BOOTSTRAP_SIZE_SM = "sm"
-    BOOTSTRAP_SIZE_LG = "lg"
+    BOOTSTRAP_SIZE_SM = "sm";
+    BOOTSTRAP_SIZE_LG = "lg";
 
-    MODE_DATE = "date"
-    MODE_TIME = "time"
-    MODE_DATETIME = "datetime"
+    MODE_DATE = "date";
+    MODE_TIME = "time";
+    MODE_DATETIME = "datetime";
 
-    VIEW_MODE_DAYS = "days"
-    VIEW_MODE_MONTHS = "months"
-    VIEW_MODE_YEARS = "years"
+    VIEW_MODE_DAYS = "days";
+    VIEW_MODE_MONTHS = "months";
+    VIEW_MODE_YEARS = "years";
 
-    VIEW_MODE_TIME = "time"
-    VIEW_MODE_HOURS = "hours"
-    VIEW_MODE_MINUTES = "minutes"
-    VIEW_MODE_SECONDS = "seconds"
+    VIEW_MODE_TIME = "time";
+    VIEW_MODE_HOURS = "hours";
+    VIEW_MODE_MINUTES = "minutes";
+    VIEW_MODE_SECONDS = "seconds";
 
-    INPUT_FORMAT_DATE = "L"
-    INPUT_FORMAT_TIME = "LT"
-    INPUT_FORMAT_DATETIME = "L LT"
+    INPUT_FORMAT_DATE = "L";
+    INPUT_FORMAT_TIME = "LT";
+    INPUT_FORMAT_DATETIME = "L LT";
 
-    VIEW_DATE = "date"
-    VIEW_TIME = "time"
+    VIEW_DATE = "date";
+    VIEW_TIME = "time";
 
-    PLACEMENT_TOP = "top"
-    PLACEMENT_BOTTOM = "bottom"
+    PLACEMENT_TOP = "top";
+    PLACEMENT_BOTTOM = "bottom";
 
-    static props = {}
+    static props = {};
 
     getProp (name) {
         return Config.props[name]
